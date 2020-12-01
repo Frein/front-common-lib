d886faed6ec24d120027d07cf5c3ee4aaf374803
Sergei Ovchinnikov <s.y.ovchinnikov@anoitr.ru>
2020-12-01 12:02:57 +0300

    first Commit

---
+++ entities/commonError.ts
@@ -0,0 +1,6 @@
+export default interface ICommonError {
+    code: string,
+    message: string,
+    name: string,
+    isSystem: boolean
+}
---
+++ index.js
@@ -0,0 +1,1 @@
+export { default as ICommonError } from './entities/commonError'
